9e6810ca12d9cbc7075f15a9d4aa24825e7f8366
Whittle <matt.whittle10@gmail.com>
2019-06-18 15:02:27 -0600

    renderButton function created

--- assets/java.js
+++ assets/java.js
@@ -1,13 +1,39 @@
 var gifs = [];
 console.log(gifs)
 
-$("button").on("click", function() {
-    var person = $(this).attr("data-person");
+function displayGifInfo() {
+
+    var person = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
       person + "&api_key=JNNrN9vGKOJ2ko1XaUFxMLuZy4M8qTrs";
-      
+    
     $.ajax({
       url: queryURL,
       method: "GET"
-    })
-  });
+    }).then(function(response) {
+      //create a new div to hold the gif
+      var newDiv = $("<div>")
+      .addClass = $("addedGif")
+      //retrieve the rating information
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response){
+        console.log(response.Rated);
+        var pOne = $("<p>").text("Rating: " + response.Rated);
+      })
+    });
+}
+
+function renderButtons() {
+  $("#buttons").empty();
+  for (var i = 0; i < gifs.length; i++){
+    var a = $("<button>");
+    a.addClass("addedGif");
+    a.attr("data-name", gifs[i]);
+    a.text(gifs[i]);
+    $("#buttons").append(a);
+  }
+}
+    
+  
