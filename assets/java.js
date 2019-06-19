86051a8bbb18ea44765fc0c8d9dd5609956d8c42
Whittle <matt.whittle10@gmail.com>
2019-06-19 14:36:06 -0600

    gifs printing to the console.

--- assets/java.js
+++ assets/java.js
@@ -3,26 +3,14 @@ console.log(gifs)
 
 function displayGifInfo() {
 
-    var person = $(this).attr("data-name");
+    var person = $(this).attr("data-person");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      person + "&api_key=JNNrN9vGKOJ2ko1XaUFxMLuZy4M8qTrs";
+        person + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10"
     
     $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function(response) {
-      //create a new div to hold the gif
-      var newDiv = $("<div>")
-      .addClass = $("addedGif")
-      //retrieve the rating information
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response){
-        console.log(response.Rated);
-        var pOne = $("<p>").text("Rating: " + response.Rated);
-      })
-    });
+    })
 }
 
 function renderButtons() {
@@ -45,6 +33,36 @@ renderButtons();
 
 $(document).click(".addedGif", displayGifInfo);
 renderButtons();
+
+$("#buttons").click(function(){
+  var person = $(this).attr("data-person");
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        person + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+  //unwrap the object
+    .then(function(response) {
+      //results is an array so we have to have a for loop
+      var results = response.data;
+      console.log(results);
+
+      for (var i = 0; i < results.length; i++) {
+        var newDiv = $("<div>");
+        var rating = results [i].rating;
+        var p = $("<p>").text("Rating: " + rating);
+        var personImage = $("<img>");
+        personImage.attr("src", results[i].images.fixed_height.url);
+        newDiv.prepend(p);
+        newDiv.prepend(personImage);
+        $("gifs").prepend(newDiv);
+      }    
+});
+});
+
+
 
 
     
